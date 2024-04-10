43e50f898ce1a7ed6f2abc80c65df47bfdcd6dd6
Konstantin_Shishin <shishinkostya2000@gmail.com>
2024-04-10 22:54:25 +0700

    initial commit

---
+++ app.py
@@ -0,0 +1,75 @@
+from flask import Flask
+from flask import request, render_template
+
+# Создаем экземпляр Flask с названием приложения
+app = Flask(__name__)
+
+# Словарь с информацией о пользователях
+UID = {
+    '1': {
+        'name': 'Евгений',
+        'city': 'Красноярск',
+        'age': 14
+    },
+    '2': {
+        'name': 'Иван',
+        'city': 'Находка',
+        'age': 16
+    },
+    '3': {
+        'name': 'Софья',
+        'city': 'Калининград',
+        'age': 15
+    }
+}
+
+# Определение маршрута для главной страницы
+@app.route('/')
+def index():
+    return render_template('page.html', context=UID)
+
+# Определение маршрута для страницы с таблицей пользователей
+@app.route('/table_users')
+def table_users():
+    return render_template('users.html')
+
+# Определение маршрута для отображения информации о конкретном пользователе
+@app.route('/user/<id>')
+def user(id):
+    if id in UID:
+        return f"Здравствуйте, Ваше имя: {UID[id]['name']}, Ваш город: {UID[id]['city']}, Ваш возраст: {UID[id]['age']}"
+    else:
+        return 'Такого пользователя не существует'
+
+# Определение маршрута для отображения информации о всех пользователях
+@app.route('/users')
+def users():
+    a = ''
+    for i in range(1, len(UID) + 1):
+        a += f"Имя: {UID[str(i)]['name']}, Город: {UID[str(i)]['city']}, Возраст: {UID[str(i)]['age']}\n"
+    return a
+
+# Определение маршрута для добавления нового пользователя
+@app.route('/add_user')
+def add_user():
+    # Получение данных о новом пользователе из запроса
+    name = request.args.get('name')
+    city = request.args.get('city')
+    age = request.args.get('age')
+    
+    # Генерация уникального ID для нового пользователя
+    new_id = str(int(max(UID)) + 1)
+    
+    # Добавление нового пользователя в словарь UID
+    UID[new_id] = {
+        'name': name,
+        'city': city,
+        'age': age
+    }
+    
+    # Возвращение информации о добавленном пользователе в виде строки
+    return f"Имя = {name}; Город = {city}; Возраст = {age}"
+
+# Запуск приложения
+if __name__ == '__main__':
+    app.run(debug=True)

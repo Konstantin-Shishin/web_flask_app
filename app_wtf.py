0d5de2d846e51ea13879ce0d68f6f781f533a546
Konstantin_Shishin <shishinkostya2000@gmail.com>
2024-05-02 17:22:19 +0700

    add FileUploder

--- app_wtf.py
+++ app_wtf.py
@@ -1,12 +1,18 @@
 # Импорт необходимых модулей из библиотек Flask и Flask-WTF
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from flask_wtf import FlaskForm
 
 # Импорт классов полей из модуля wtforms
-from wtforms import StringField, IntegerField, SubmitField, TelField, RadioField
+from wtforms import StringField, IntegerField, SubmitField, TelField, RadioField, FileField
 
 # Импорт валидаторов из модуля wtforms.validators
 from wtforms.validators import InputRequired
+
+from flask_wtf.file import FileField, FileRequired
+
+from werkzeug.utils import secure_filename
+
+import os
 
 # Создаем экземпляр Flask с названием приложения
 app = Flask(__name__)
@@ -69,6 +75,8 @@ class RegistrationForm(FlaskForm):
     
     # Поле для ввода комментария без валидации
     comment = StringField()
+
+    file = FileField(validators=[FileRequired()])
     
     # Кнопка отправки формы
     submit = SubmitField(label=('Submit'))
@@ -91,10 +99,16 @@ def registration():
     # Проверяем, была ли форма отправлена и прошла ли валидацию
     if form.validate_on_submit():
         # Если форма прошла валидацию, получаем данные из полей формы
-        email, phone, name, address, pol, age = form.email.data, form.phone.data, form.name.data, form.address.data, form.pol.data, form.age.data 
+        email, phone, name, address, pol, age, file = form.email.data, form.phone.data, form.name.data, form.address.data, form.pol.data, form.age.data, form.file.data
         
         # Выводим данные формы в консоль для отладки
         print(email, phone, name, address, pol, age)
+
+        
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(
+                app.static_folder, 'photos', filename
+        ))
 
         # Добавление пользователя в словарь
         UID[str(int(max(UID))+1)]= {
@@ -103,11 +117,13 @@ def registration():
         'age': age,
         'email': email,
         'pol': pol,
-        'phone': phone
-        }   
+        'phone': phone,
+        'filename': filename
+        } 
 
         # Возвращаем приветственное сообщение с использованием имени пользователя
-        return f'Hello {name} welcome to our site!'
+        # return f'Hello {name} welcome to our site! {filename}'
+        return render_template('welcome.html', fname = filename)
     
     # Если форма не была отправлена или не прошла валидацию,
     # отображаем HTML-шаблон с формой регистрации,
